736376755c297db9e03a745053441cd574cd3dd9
Sophie <sophiedodsworth@hotmail.com>
2023-01-27 22:11:42 +0000

    creating an ajax call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
 // declaring global variables
 
 var APIkey = "8f2d65f42c8a54965d177a0074d59cb7"
-var openWeatherAPI = 'https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}'
+var openWeatherURL = 'https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}'
+
+// Creating an AJAX call for the search button being clicked
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(queryURL)
+    console.log(response)
+});
