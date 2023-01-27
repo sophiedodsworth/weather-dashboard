df8abff8b59b88f3bcf8008ecd0e8245d6d588fa
Sophie <sophiedodsworth@hotmail.com>
2023-01-27 20:32:00 +0000

    declaring global variables

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// declaring global variables
+
+var APIkey = "8f2d65f42c8a54965d177a0074d59cb7"
+var openWeatherAPI = 'https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}'
